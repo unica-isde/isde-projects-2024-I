1d7cc3c0d15a185230ef8acc118c828e4d065222
Ahsan <oyeslow@gmail.com>
2025-04-11 22:58:52 +0200

    Fix #1: Completed histogram feature with form and route

--- app/forms/histogram_form.py
+++ app/forms/histogram_form.py
@@ -1,16 +1,16 @@
 from fastapi import Request
 
-class HistogramRequestForm:
-    def __init__(self, incoming_request: Request) -> None:
-        self.incoming_request: Request = incoming_request
-        self.validation_errors: list = []
-        self.selected_image_name: str = ""
+class HistogramForm:
+    def __init__(self, request: Request) -> None:
+        self.request: Request = request
+        self.errors: list = []
+        self.image_id: str = ""
 
-    async def parse_form_data(self):
-        submitted_form = await self.incoming_request.form()
-        self.selected_image_name = submitted_form.get("image_id")
+    async def load_data(self):
+        form = await self.request.form()
+        self.image_id = form.get("image_id")
 
-    def validate(self):
-        if not self.selected_image_name or not isinstance(self.selected_image_name, str):
-            self.validation_errors.append("Please select a valid image name.")
-        return len(self.validation_errors) == 0
+    def is_valid(self):
+        if not self.image_id or not isinstance(self.image_id, str):
+            self.errors.append("A valid image id is required")
+        return not self.errors

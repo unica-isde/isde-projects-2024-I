648b0f5c0a24cf67ae6aee4b570741fbb89ffc11
Ahsan <oyeslow@gmail.com>
2025-04-13 19:21:13 +0200

    Fix #5: Add different extensions funcationalities in utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,13 +1,20 @@
 import os
-
 from app.config import Configuration
 
 conf = Configuration()
 
-
 def list_images():
-    """Returns the list of available images."""
     img_names = filter(
         lambda x: x.endswith(".JPEG"), os.listdir(conf.image_folder_path)
     )
     return list(img_names)
+
+async def add_image_to_list(image, name: str) -> bool:
+    if not name.lower().endswith((".jpeg", ".jpg", ".png")):
+        return False
+
+    save_path = os.path.join(conf.image_folder_path, name)
+    with open(save_path, "wb") as f:
+        f.write(await image.read())
+
+    return True
